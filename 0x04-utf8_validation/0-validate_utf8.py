09ef8c9474bffbd44127b87a02b0ca695a6537c5
maissaranejmi1999 <molinmay1999@gmail.com>
2024-10-31 15:33:44 +0100

    validate_utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+def validUTF8(data):
+	data_len = len(data)
+	i = 0
+	sequence = 0
+	
+	while i < data_len:
+		if data[i] & 0b10000000 == 0:
+			sequence = 1
+		elif data[i] & 0b11100000 == 0b11000000:
+			sequence = 2
+		elif data[i] & 0b11110000 == 0b11100000:
+			sequence = 3
+		elif data[i] & 0b11111000 == 0b11110000:
+			sequence = 4
+		else:
+			return False
+	
+		if i + sequence > data_len:
+			return False
+
+		for j in range(1, sequence):
+			if data[i + j] & 0b11000000 != 0b10000000:
+				return False
+		i += sequence
+	return True
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,2 @@
+This reposetory is concerning UTF-8 validation.
+The UTF-8 validation checks if a sequence of bytes follows the UTF-8 encoding rules, wich is important in processing, as malformed UTf-8 sequences can cause errors or security vulnerabilities
