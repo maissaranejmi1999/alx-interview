027b29f736e2b16d772b5280e2e1a58026fc316e
maissaranejmi1999 <molinmay1999@gmail.com>
2024-10-31 15:48:09 +0100

    validate_utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 def validUTF8(data):
-	data_len = len(data)
-	i = 0
-	sequence = 0
-	
-	while i < data_len:
-		if data[i] & 0b10000000 == 0:
-			sequence = 1
-		elif data[i] & 0b11100000 == 0b11000000:
-			sequence = 2
-		elif data[i] & 0b11110000 == 0b11100000:
-			sequence = 3
-		elif data[i] & 0b11111000 == 0b11110000:
-			sequence = 4
-		else:
-			return False
-	
-		if i + sequence > data_len:
-			return False
+    data_len = len(data)
+    i = 0
 
-		for j in range(1, sequence):
-			if data[i + j] & 0b11000000 != 0b10000000:
-				return False
-		i += sequence
-	return True
+    while i < data_len:
+        # Determine the length of the sequence
+        if data[i] & 0b10000000 == 0:
+            sequence = 1
+        elif data[i] & 0b11100000 == 0b11000000:
+            sequence = 2
+        elif data[i] & 0b11110000 == 0b11100000:
+            sequence = 3
+        elif data[i] & 0b11111000 == 0b11110000:
+            sequence = 4
+        else:
+            return False
+
+        # Check if remaining bytes are enough for the sequence
+        if i + sequence > data_len:
+            return False
+
+        # Validate the continuation bytes (10xxxxxx)
+        for j in range(1, sequence):
+            if data[i + j] & 0b11000000 != 0b10000000:
+                return False
+
+        # Move to the next character in the sequence
+        i += sequence
+
+    return True
